05a79d488362d5dc2898a9e9e5e3b29e9be203cd
Kevin Portela <thekevinportela@gmail.com>
2022-08-24 19:27:54 -0400

    added some project pics and framer motion

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,4 +1,5 @@
 import { Box, Center, Image, Text } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
 const Work = () => {
   return (
@@ -16,26 +17,45 @@ const Work = () => {
         Check out some of my projects!
       </Text>
       <Center mt={5}>
-        <Image
-          src={require("../assets/imgs/NumberGuesserThumbnail.jpg")}
-          h={300}
-          w={450}
-          objectFit={"cover"}
-        />
-        <Box
-          border={"1px"}
-          borderColor={"#000"}
-          h={300}
-          w={450}
-          bg={"#ccc"}
-        ></Box>
-        <Box
-          border={"1px"}
-          borderColor={"#000"}
-          h={300}
-          w={450}
-          bg={"#ccc"}
-        ></Box>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: false, amount: 0.2 }}
+          whileHover={{ scale: 1.2 }}
+        >
+          <Image
+            src={require("../assets/imgs/NumberGuesser.jpeg")}
+            h={300}
+            w={450}
+            objectFit={"cover"}
+          />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: false, amount: 0.5 }}
+          whileHover={{ scale: 1.2, borderRadius: 20 }}
+        >
+          <Image
+            src={require("../assets/imgs/WeatherApp.jpeg")}
+            h={300}
+            w={450}
+            objectFit={"cover"}
+          />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: false, amount: 0.8 }}
+          whileHover={{ scale: 1.2 }}
+        >
+          <Image
+            src={require("../assets/imgs/TodoApp.jpeg")}
+            h={300}
+            w={450}
+            objectFit={"cover"}
+          />
+        </motion.div>
       </Center>
       <Center>
         <Box
